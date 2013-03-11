0aeade564e1a80c7200d76fdaf4ddcf3d6ee56fd
reggie <reggie-ohm@emailzero.com>
2013-03-11 16:49:10 -0700

    Only require the crashlytics script to run for non-Debug builds.

--- OhmPlayer3/OhmPlayer4/scripts/build_phase_crashlytics.sh
+++ OhmPlayer3/OhmPlayer4/scripts/build_phase_crashlytics.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ "$CONFIGURATION" != "Debug" ]
+then
+
 echo RUNNING CRASHLYTICS SCRIPT
 
 ../Third\ Party/Crashlytics/Crashlytics.framework/run 7ca949e35438fc0d1e7a9c0cd2c2adfb5b7ce6df
+fi
