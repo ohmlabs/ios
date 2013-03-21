1a9950f1bddbff532ed78a5c2ccc197d954a51d2
reggie <reggie-ohm@emailzero.com>
2013-03-21 10:30:34 -0700

    ALWAYS enable crashytics and upload symbol information at build time. Note: if Xcode is running, Crashlytics will NOT send a crash log, so it should be OK to leave on for developers during development. If it's a problem, we can always turn it back off...

--- OhmPlayer3/OhmPlayer4/scripts/build_phase_crashlytics.sh
+++ OhmPlayer3/OhmPlayer4/scripts/build_phase_crashlytics.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-if [ "$CONFIGURATION" != "Debug" ]
-then
+UploadCrashlyticsData () {
 
 echo RUNNING CRASHLYTICS SCRIPT
 
 ../Third\ Party/Crashlytics/Crashlytics.framework/run 7ca949e35438fc0d1e7a9c0cd2c2adfb5b7ce6df
+
+}
+
+if [ "$CONFIGURATION" != "Debug" ]
+then
+
+UploadCrashlyticsData
+
+elif [ "$CONFIGURATION" == "Debug" ]
+then
+
+UploadCrashlyticsData
+#echo WARNING: DID NOT RUN CRASHLYTICS FOR CONFIGURATION $CONFIGURATION
+
 fi
